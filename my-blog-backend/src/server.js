5cb045cf37a667f2700399e0e51a55c92519bb71
Nikhil-L <nikhil89515@gmail.com>
2023-12-11 10:02:31 +0530

    Added the backend server code

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,15 +1,53 @@
 import express from 'express';
 
+let articlesInfo = [{
+    name: 'learn-react',
+    upvote: 0,
+    comments: [],
+},{
+    name: 'learn-node',
+    upvote: 0,
+    comments: [],
+},{
+    name: 'mongodb',
+    upvote: 0,
+    comments: [],
+}]
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.post('/hello', (req, res) => {
-    console.log(1);
-    console.log(req.body);
-    res.send(`Hello ${req.body.name}!`);
-});
 
 app.listen(8000, () => {
     console.log("Server is listening on port 8000");
+});
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const {name} = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if(article)
+    {
+        article.upvote += 1;
+        res.send(`This ${name} article now has ${article.upvote} upvotes!`);
+    }
+    else{
+        res.send("The article doesn\'t exit");
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if(article)
+    {
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+    }
+    else{
+        res.send("The article doesn\'t exit");
+    }
+
 });
